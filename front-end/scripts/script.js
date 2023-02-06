09dd8e106db404f72c91246548cca4f1e647fe3f
Whitedog Warren <whitedog@lwerquin011222.intra.irium-group.com>
2023-02-06 16:22:34 +0100

    displaying users list

--- front-end/scripts/script.js
+++ front-end/scripts/script.js
@@ -2,7 +2,6 @@ const userForm = document.getElementById("userForm");
 userForm.addEventListener('submit', event => {
     event.preventDefault();
     let formData = new FormData(userForm);
-    console.log(Object.fromEntries(formData));
     postUser(Object.fromEntries(formData));
 });
 
@@ -16,18 +15,43 @@ const postUser = (data) => {
         return response.json();
     })
     .then(data => {
-        console.log(data);
+        fetchUsers();
     })
     .catch(error => {
         console.log(error);
     })
 }
 
-fetch("http://backend.localhost/users")
+const displayUsers = (users) => {
+    let userlistElt = document.getElementById('userlist');
+    userlistElt.innerHTML = "<h3>users</h3>";
+    if(users.length > 0) {
+        if(document.getElementById('empty_userlist')) {
+            document.getElementById('empty_userlist').remove();
+        }
+        for(user of users) {
+            let userElt = document.createElement('span');
+            userElt.innerText = user.pseudo;
+            userlistElt.appendChild(userElt);
+            userlistElt.appendChild(document.createElement('br'));
+        }
+    } else {
+        let emptyList = document.createElement('span');
+        emptyList.setAttribute('id', "empty_userlist");
+        emptyList.innerText = "aucun";
+        userlistElt.appendChild(emptyList);
+    }
+}
+
+const fetchUsers = () => {
+    fetch("http://backend.localhost/users")
     .then(response => {
         return response.json();
     })
     .then(users => {
-        console.log(users);
+        displayUsers(users);
     })
     .catch(error => console.log(error));
+}
+
+fetchUsers();
